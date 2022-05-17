d465dffa24be2088fd2f152c4ce8221a9f84959f
Andrey Kudlay <matroskin8@i.ua>
2022-05-17 13:30:13 +0300

    Third lesson

--- index.php
+++ index.php
@@ -7,5 +7,40 @@ require_once __DIR__ . '/funcs.php';
 $telegram = new \Telegram\Bot\Api(TOKEN);
 $updates = $telegram->getWebhookUpdates();
 
-log_updates($updates);
+//log_updates($updates);
 
+$chat_id = $updates['message']['chat']['id'] ?? '';
+$text = $updates['message']['text'] ?? '';
+if (isset($updates['message']['photo'])) {
+    $key = count($updates['message']['photo']) - 1;
+    $file_id = $updates['message']['photo'][$key]['file_id'];
+}
+
+if ($text == '/start') {
+
+    $res = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => "Привет! Я - бот-конвертер. Я умею конверировать изображения из форматов JPG или PNG в формат WebP. Просто пришлите мне изображение нужного формата, и я отправлю в ответ то же изображение в формате WebP.",
+    ]);
+
+} elseif (isset($file_id)) {
+
+    $file = $telegram->getFile([
+        'file_id' => $file_id,
+    ]);
+
+    //$file_url = "https://api.telegram.org/file/bot" . TOKEN . "/{$file['file_path']}";
+
+    $res = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => json_encode($file, JSON_PRETTY_PRINT),
+    ]);
+
+} else {
+
+    $res = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => $text,
+    ]);
+
+}
